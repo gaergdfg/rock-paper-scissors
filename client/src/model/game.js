2dd122f8326426cf719cb0277c7552f43f807cc1
gaergdfg <p.prabucki@gmail.com>
2021-04-24 18:53:45 +0200

    fix heroku deployment #5

--- client/src/model/game.js
+++ client/src/model/game.js
@@ -5,6 +5,7 @@ const socket = require('../connections/socket').socket;
 
 const port = process.env.PORT || 8000;
 const url = process.env.URL || `localhost:${port}`;
+console.log(process.env.URL);
 
 
 class Game extends React.Component {
