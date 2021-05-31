c4deafcb500083f304a8e01a1e5e44d83c91936d
mati <71319308+mj426382@users.noreply.github.com>
2021-05-31 18:47:53 +0200

    final-rps-without-front
    final-rps-without-front

--- client/src/model/game.js
+++ client/src/model/game.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import {Redirect, useParams} from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+import styles from './game.css';
+import axios from 'axios';
+import { cookies } from '../cookie-manager';
 
 const socket = require('../connections/socket').socket;
 
 const port = process.env.PORT || 8000;
 const url = process.env.URL || `localhost:${port}`;
+const hubUrl = 'https://game-nexus.herokuapp.com/';
 //const url = 'https://game-nexus-rps.herokuapp.com';
 
 
@@ -18,99 +23,242 @@ class Game extends React.Component {
 		scoreHost: 0,
 		scoreGuest: 0,
 		
-		winner: 'nobody'
+		winner: 'nobody',
+		
+		endOfGame: false
+	
 	}
 
-	updateGameState() {
-		if(this.state.choiceGuest == this.state.choiceHost) {
-			this.setState({
-				winner: 'draw'
-			});
-		} else if(this.state.choiceGuest == 1 && this.state.choiceHost == 0) {
-			this.setState({
-				winner: 'Guest'
-			});
-			this.setState({
-				scoreGuest: this.state.scoreGuest + 1
-			});
-		} else if(this.state.choiceGuest == 2 && this.state.choiceHost == 1) {
-			this.setState({
-				winner: 'Guest'
-			});
-			this.setState({
-				scoreGuest: this.state.scoreGuest + 1
-			});
-		} else if(this.state.choiceGuest == 0 && this.state.choiceHost == 2) {
-			this.setState({
-				winner: 'Guest'
-			});
-			this.setState({
-				scoreGuest: this.state.scoreGuest + 1
-			});
-		} else if(this.state.choiceGuest == 0 && this.state.choiceHost == 1) {
-			this.setState({
-				winner: 'Host'
-			});
-			this.setState({
-				scoreHost: this.state.scoreHost + 1
-			});
-		} else if(this.state.choiceGuest == 1 && this.state.choiceHost == 2) {
-			this.setState({
-				winner: 'Host'
-			});
-			this.setState({
-				scoreHost: this.state.scoreHost + 1
-			});
-		} else if(this.state.choiceGuest == 2 && this.state.choiceHost == 0) {
-			this.setState({
-				winner: 'Host'
-			});
-			this.setState({
-				scoreHost: this.state.scoreHost + 1
-			});
-		}
-	}
-
-	setWinner() {
+	componentDidMount() {
+		
+		function setWinner() {
+		console.log("MAMY TO");
 		alert('winner: ' + this.state.winner);
-		if(this.scoreGuest == 5) { // FIXME: this.scoreGuest not defined -> this.state.scoreGuest
+		if(this.state.scoreGuest == 3) { // FIXME: this.scoreGuest not defined -> this.state.scoreGuest
+		let username = cookies.get('username');
+            if (username) {
+                axios.post(hubUrl + 'api/v1/stats', {
+                    game: 'rps',
+                    username: username,
+                    result: this.stats.isHost ? 1 : 0
+                });
+                cookies.get('username', { path: '/' });
+            }
+			console.log("WESZLO GUEST");
 			window.location.href = "/guestWin.html"; // FIXME: use React Redirect instead
-		} else if(this.scoreHost == 5) {
+		} else if(this.state.scoreHost == 3) {
+			let username = cookies.get('username');
+            if (username) {
+                axios.post(hubUrl + 'api/v1/stats', {
+                    game: 'rps',
+                    username: username,
+                    result: this.stats.isHost ? 1 : 0
+                });
+                cookies.get('username', { path: '/' });
+            }
+			console.log("WESZLO HOST");
 			window.location.href = "/hostWin.html";
+			
 		}
 		this.setState({ // FIXME: set state variable to represent that the game is finished, so that render() can redirect the player to end-game screen
 			winner: 'nobody',
 			choiceGuest: -1, 
 			choiceHost: -1
 		});
-	}
-
-	componentDidMount() {
-		this.setState({
-			isHost: this.props.isHost
-		});
+		}
+		
+		console.log('props:', this.props);
+		console.log(this.state);
+			this.setState({
+				isHost: this.props.isHost
+			})
 
 		socket.on("hostMove", choice => {
+			console.log("hostMoveEvent", choice);
+			console.log(this.state.isHost);
 			if(!this.state.isHost) {
-				this.setState({
-					choiceHost: choice
-				});
+			this.setState({
+				choiceHost: choice
+			})
 			}
 			if(this.state.choiceGuest != -1 && this.state.choiceHost != -1) {
-				this.updateGameState();
-				this.setWinner();
+				if(this.state.choiceGuest == this.state.choiceHost) {
+					this.setState({
+						winner: 'draw'
+					})
+					console.log("sc: " + this.state.scoreGuest);
+					
+				}
+				else if(this.state.choiceGuest == 1 && this.state.choiceHost == 0) {
+					this.setState({
+						winner: 'Guest'
+					})
+					this.setState({
+						scoreGuest: (this.state.scoreGuest + 1)
+					})
+					console.log("sc: " + this.state.scoreGuest);
+				}
+				else if(this.state.choiceGuest == 2 && this.state.choiceHost == 1) {
+					this.setState({
+						winner: 'Guest'
+					})
+					this.setState({
+						scoreGuest: (this.state.scoreGuest + 1)
+					})
+					console.log("sc: " + this.state.scoreGuest);
+				}
+				else if(this.state.choiceGuest == 0 && this.state.choiceHost == 2) {
+					this.setState({
+						winner: 'Guest'
+					})
+					this.setState({
+						scoreGuest: (this.state.scoreGuest + 1)
+					})
+					console.log("sc: " + this.state.scoreGuest);
+				}
+				else if(this.state.choiceGuest == 0 && this.state.choiceHost == 1) {
+					this.setState({
+						winner: 'Host'
+					})
+					this.setState({
+						scoreHost: (this.state.scoreHost + 1)
+					})
+				}
+				else if(this.state.choiceGuest == 1 && this.state.choiceHost == 2) {
+					this.setState({
+						winner: 'Host'
+					})
+					this.setState({
+						scoreHost: (this.state.scoreHost + 1)
+					})
+				}
+				else if(this.state.choiceGuest == 2 && this.state.choiceHost == 0) {
+					this.setState({
+						winner: 'Host'
+					})
+					this.setState({
+						scoreHost: (this.state.scoreHost + 1)
+					})
+				}
+				
+				window.alert("the winner is " + this.state.winner);
+				console.log(this.state.scoreHost)
+				if(this.state.scoreHost == 3) {
+					console.log("koniec!!!");
+					this.setState({
+						endOfGame: true
+					})
+					setWinner();
+					//tutaj dodac do rankimgu i przejscie moze jakies.
+				}
+				console.log(this.state.scoreGuest);
+				if(this.state.scoreGuest == 3) {
+					console.log("koniec!!!");
+					this.setState({
+						endOfGame: true
+					})
+					setWinner();
+					//tutaj dodac do rankimgu i przejscie moze jakies.
+				}
+				this.setState({
+						winner: 'nobody',
+						choiceGuest: -1, 
+						choiceHost: -1
+					})
 			}
 		})
+		
 
 		socket.on("guestMove", choice => {
+			console.log("guestMoveEvent", choice);
+			console.log(this.state.isHost);
 			if(this.state.isHost) {
-				this.setState({
-					choiceGuest: choice
-				});
+			this.setState({
+				choiceGuest: choice
+			})
 			}
 			if(this.state.choiceGuest != -1 && this.state.choiceHost != -1) {
-				this.updateGameState();
-				this.setWinner();
+				if(this.state.choiceGuest == this.state.choiceHost) {
+					this.setState({
+						winner: 'draw'
+					})
+					
+				}
+				else if(this.state.choiceGuest == 1 && this.state.choiceHost == 0) {
+					this.setState({
+						winner: 'Guest'
+					})
+					this.setState({
+						scoreGuest: (this.state.scoreGuest + 1)
+					})
+					console.log("sc: " + this.state.scoreGuest);
+				}
+				else if(this.state.choiceGuest == 2 && this.state.choiceHost == 1) {
+					this.setState({
+						winner: 'Guest'
+					})
+					this.setState({
+						scoreGuest: (this.state.scoreGuest + 1)
+					})
+					console.log("sc: " + this.state.scoreGuest);
+				}
+				else if(this.state.choiceGuest == 0 && this.state.choiceHost == 2) {
+					this.setState({
+						winner: 'Guest'
+					})
+					this.setState({
+						scoreGuest: (this.state.scoreGuest + 1)
+					})
+					console.log("sc: " + this.state.scoreGuest);
+				}
+				else if(this.state.choiceGuest == 0 && this.state.choiceHost == 1) {
+					this.setState({
+						winner: 'Host'
+					})
+					this.setState({
+						scoreHost: (this.state.scoreHost + 1)
+					})
+				}
+				else if(this.state.choiceGuest == 1 && this.state.choiceHost == 2) {
+					this.setState({
+						winner: 'Host'
+					})
+					this.setState({
+						scoreHost: (this.state.scorHost + 1)
+					})
+				}
+				else if(this.state.choiceGuest == 2 && this.state.choiceHost == 0) {
+					this.setState({
+						winner: 'Host'
+					})
+					this.setState({
+						scoreHost: (this.state.scoreHost + 1)
+					})
+				}
+				window.alert("the winner is " + this.state.winner);
+				console.log(this.state.scoreHost)
+				if(this.state.scoreHost == 3) {
+					console.log("koniec!!!");
+					this.setState({
+						endOfGame: true
+					})
+					setWinner();
+					//tutaj dodac do rankimgu i przejscie moze jakies.
+				}
+				console.log(this.state.scoreGuest);
+				if(this.state.scoreGuest == 3) {
+					console.log("koniec!!!");
+					this.setState({
+						endOfGame: true
+					})
+					setWinner();
+					//tutaj dodac do rankimgu i przejscie moze jakies.
+				}
+				this.setState({
+						winner: 'nobody',
+						choiceGuest: -1, 
+						choiceHost: -1
+					})
 			}
 		})
 		
@@ -118,57 +266,66 @@ class Game extends React.Component {
 	
 
 	render() {
-		// FIXME: redirect the player to the end-game screen if a state variable was set
 		return (
-			<div class="box">
-				<div class="row">
+		<React.Fragment>
+		{
+			this.state.endOfGame ?
+				<Redirect to = {"https://game-nexus.herokuapp.com/"}> 
+				</Redirect>
+			:
+			<div className={styles.box}>
+				<div className={styles.row}>
 				<button onClick={this.choose.bind(this, this.state.host, 0)}>rock</button>
 				</div>
-				<div class="row">
+				<div className={styles.row}>
 				<button onClick={this.choose.bind(this, this.state.host, 1)}>paper</button>
 				</div>
-				<div class="row">
+				<div className={styles.row}>
 				<button onClick={this.choose.bind(this, this.state.host, 2)}>scissors</button>
 				</div>
 				Host:
 				<div>
-				<output>{this.state.choiceHost}</output>
-				</div>
-				<div>
+				<h6>Score:</h6>
 				<output>{this.state.scoreHost}</output>
 				</div>
 				Guest:
 				<div>
-				<output>{this.state.choiceGuest}</output>
-				</div>
-				<div>
+				<h6>Score:</h6>
 				<output>{this.state.scoreGuest}</output>
 				</div>
 			</div>
+		}
+		</React.Fragment>
 		)
 	}
 	
-	/**
+	
+	/*
 	 * Passes information about player's choice to the game.
 	 * @param {boolean} isHost 
 	 * @param {0|1|2} choice (0 - rock), (1 - paper), (2 - scissors).
 	 */
 	choose(isHost, choice) {
-		if (isHost) {    
+		
+		if(this.state.endOfGame == true) {
+			console.log("koniec");
+		}
+		else if (isHost) {    
 			this.setState({
 				choiceHost: choice
-			});
+			})
 			socket.emit("new host move", {choice: choice, gameId: this.props.gameId});
 			console.log("inH");
 		}
 		else {
 			this.setState({
 				choiceGuest: choice
-			});
+			})
 			socket.emit("new guest move", {choice: choice, gameId: this.props.gameId});
 			console.log("inG");
 		}
 	}
+
 }	
 
 const GameWrapper = (props) => {
